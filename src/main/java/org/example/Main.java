967211480d50262e72db2b86915bc5f1c318ce03
wasabikun1704 <sbk283@naver.com>
2023-09-19 10:37:14 +0900

    while exam

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
-// 문제 : 1부터 5까지 출력해주세요.
+// 문제 : -100부터 25까지 출력해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int i = 1;
-        while (i<=5) {
+        int i = -100;
+        while (i <= 25) {
             System.out.println(i);
             i++;
         }
