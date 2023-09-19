1f869b643f2f22c4a7933f9c956e5b2ce85e4200
wasabikun1704 <sbk283@naver.com>
2023-09-19 11:08:45 +0900

    1~3 10번출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,49 @@
-// -100부터 25까지의 합을 더한 결과를 출력해주세요.
+// 문제 : 1부터 3까지 출력하는 작업을 10번 해주세요.
+// 조건 : 2중 while문 사용
 
 class Main {
     public static void main(String[] args) {
-        int sum = 0;
-        int start = -100;
-        int end = 25;
+        System.out.println("Hello world!");
 
-        int i = 1;
-        while (i <= end) {
-            sum += i;
+        int i = 0;
+        while (i <= 9) {
+            int j = 1;
+            while (j<=3) {
+                System.out.println(j);
+                j++;
+            }
             i++;
         }
-        System.out.println("sum : " + sum);
+        // 출력
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
     }
 }
