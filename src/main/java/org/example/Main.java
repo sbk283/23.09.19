998ac4e0d294b7bd330c6a4f832c415a9f30d620
wasabikun1704 <sbk283@naver.com>
2023-09-19 10:41:13 +0900

    while 구구단

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
-// 문제 : -100부터 25까지 출력해주세요.
+// 문제 : 아래와 같이 출력해주세요.
+// 문제 : 단을 * 1000까지 출력해주세요.
+// 조건 : 곱하는 순서를 거꾸로 해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int i = -100;
-        while (i <= 25) {
-            System.out.println(i);
-            i++;
+        int dan = 8;
+
+        int i = 1000;
+
+        while (i>=1){
+            System.out.println(dan + " * " + i + " = " + dan * i);
+            i--;
         }
     }
 }
