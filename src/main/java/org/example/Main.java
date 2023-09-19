7cb9d43e75ebce07880c75a801848e33869636ea
wasabikun1704 <sbk283@naver.com>
2023-09-19 11:04:42 +0900

    -100부터 25까지의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,16 @@
-// 1부터 5까지의 합을 더한 결과를 출력해주세요.
+// -100부터 25까지의 합을 더한 결과를 출력해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int i = 1;
         int sum = 0;
+        int start = -100;
+        int end = 25;
 
-        while(i<=5) {
+        int i = 1;
+        while (i <= end) {
             sum += i;
             i++;
         }
-        System.out.println(sum);
+        System.out.println("sum : " + sum);
     }
 }
