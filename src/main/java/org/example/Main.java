a73cac354e8f7f30585db1bd5aefe04c3d471bdc
wasabikun1704 <sbk283@naver.com>
2023-09-19 10:21:16 +0900

    third

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,19 @@
 // 문제 : 아래와 같이 출력해주세요.
-// 문제 : dan 변수의 값에 따라 첫번째로 곱하는 수가 변하게 해주세요.
+// 문제 : dan 변수의 값에 따라 그에 해당하는 구구단이 출력 되도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int dan = 8;
+        int dan = 9;
 
-        System.out.println(dan + " * 1 = 8");
-        System.out.println(dan + " * 2 = 16");
-        System.out.println(dan + " * 3 = 24");
-        System.out.println(dan + " * 4 = 32");
-        System.out.println(dan + " * 5 = 40");
-        System.out.println(dan + " * 6 = 48");
-        System.out.println(dan + " * 7 = 56");
-        System.out.println(dan + " * 8 = 64");
-        System.out.println(dan + " * 9 = 72");
+        System.out.println(dan + " * 1 = " + dan * 1);
+        System.out.println(dan + " * 2 = " + dan * 2);
+        System.out.println(dan + " * 3 = " + dan * 3);
+        System.out.println(dan + " * 4 = " + dan * 4);
+        System.out.println(dan + " * 5 = " + dan * 5);
+        System.out.println(dan + " * 6 = " + dan * 6);
+        System.out.println(dan + " * 7 = " + dan * 7);
+        System.out.println(dan + " * 8 = " + dan * 8);
+        System.out.println(dan + " * 9 = " + dan * 9);
 
         // 출력
     /*
