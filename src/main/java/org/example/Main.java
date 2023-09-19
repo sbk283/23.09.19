2f4db691d0baffdb0dd1f5051dddb96f3c206ab2
wasabikun1704 <sbk283@naver.com>
2023-09-19 10:35:33 +0900

    구구단 while second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,10 @@
-// 문제 : 아래와 같이 출력해주세요.
-// 문제 : 단을 * 1000까지 출력해주세요.
+// 문제 : 1부터 5까지 출력해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int dan = 8;
-
         int i = 1;
-        while (i <= 1000) {
-            System.out.println(dan + " * " + i + " = " + dan * i);
+        while (i<=5) {
+            System.out.println(i);
             i++;
         }
     }
