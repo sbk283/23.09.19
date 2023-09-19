6a824dc073b47c657911452ea56a4e979c88de4a
wasabikun1704 <sbk283@naver.com>
2023-09-19 20:22:34 +0900

    a와b변수를 c를 사용해서 위치옮기기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,49 +1,33 @@
-// 문제 : 1부터 3까지 출력하는 작업을 10번 해주세요.
-// 조건 : 2중 while문 사용
+// 문제 : a와 b가 가지고 있는 값을 서로 뒤바꿔주세요. // 조건 : 숫자와 사칙연산 사용 금지
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // `int a` => a라는 이름의 변수(즉 공간)을 만든다.
+        // `int` => a라는 공간에는 정수만 담을 수 있다.
+        // `;` => 문장이 끝남을 알린다.
+        int a;
+        a = 5;
 
-        int i = 0;
-        while (i <= 9) {
-            int j = 1;
-            while (j<=3) {
-                System.out.println(j);
-                j++;
-            }
-            i++;
-        }
-        // 출력
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
+        System.out.println(a);
+        System.out.println(a + 10);
+
+        int b = 10;
+
+        // `+` => 문장과 문자을 합친다.
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+
+        // 여기서 부터
+        int c;
+        c = a; //c의 빈곳에 a값을 집어넣는다.
+        a = b; //a의 빈곳에 b값을 집어넣는다.
+        b = c; //b의 값에 a값을 옮긴 c값을 집어넣는다.
+        // X = Y; X : 옮길값, Y : 받을값
+        // 여기까지 수정 가능
+
+        System.out.println("a : " + a);
+        // 출력 : a : 10
+        System.out.println("b : " + b);
+        // 출력 :  b : 5
     }
 }
